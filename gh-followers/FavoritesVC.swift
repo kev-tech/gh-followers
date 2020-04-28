98173c1180cd1c24b00962f33c8581f7967441b6
KC <kc@kevtech.co>
2020-04-28 16:56:15 -0400

    Initial commit,

---
+++ gh-followers/FavoritesVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesVC.swift
+//  gh-followers
+//
+//  Created by whiis on 4/22/20.
+//  Copyright © 2020 kevtech. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ gh-followers/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVC.swift
+//  gh-followers
+//
+//  Created by whiis on 4/22/20.
+//  Copyright © 2020 kevtech. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}
