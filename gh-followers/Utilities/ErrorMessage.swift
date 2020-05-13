c19494b58ca01b5d1066938e4829398c7250f467
KC <kc@kevtech.co>
2020-05-12 20:29:14 -0400

    Update project to use Result.

---
+++ gh-followers/Utilities/ErrorMessage.swift
@@ -0,0 +1,16 @@
+//
+//  ErrorMessage.swift
+//  gh-followers
+//
+//  Created by whiis on 5/12/20.
+//  Copyright © 2020 kevtech. All rights reserved.
+//
+
+import Foundation
+
+enum GFError: String, Error{
+    case invalidUsername = "This username created an invalid request. Please try again."
+    case unableToCompleteRequest = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse = "Invalid response from server. Please try again."
+    case invalidData = "The data received from the server was invalid. Please try again."
+}
