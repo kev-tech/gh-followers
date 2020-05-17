d16eb401d2b3b3f472baedb05702bf12506f53df
KC <kc@kevtech.co>
2020-05-17 18:08:42 -0400

    Add activity indicator for network calls.

--- gh-followers/Extensions/UIViewController+Ext.swift
+++ gh-followers/Extensions/UIViewController+Ext.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+fileprivate var containerView: UIView!
+
 extension UIViewController {
    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
         DispatchQueue.main.async {
@@ -15,6 +17,35 @@ extension UIViewController {
             alertVC.modalPresentationStyle = .overFullScreen
             alertVC.modalTransitionStyle = .crossDissolve
             self.present(alertVC, animated: true)
+        }
+    }
+    
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.25) { containerView.alpha = 0.8 }
+        
+        let activityIndicatior = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicatior)
+        
+        activityIndicatior.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            activityIndicatior.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            activityIndicatior.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        activityIndicatior.startAnimating()
+    }
+    
+    func dismissLoadingView() {
+        DispatchQueue.main.async {
+             containerView.removeFromSuperview()
+            containerView = nil
         }
     }
 }
