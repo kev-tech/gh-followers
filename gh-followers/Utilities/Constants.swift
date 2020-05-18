988e489fad6787ba8d2759052c5116c64ced5e5f
KC <kc@kevtech.co>
2020-05-18 15:20:14 -0400

    Add Constants helper. Add GFSecondaryTitleLabel. Add GFUSerInfoHeaderVC. Add header view to UserInfoVC.

---
+++ gh-followers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  gh-followers
+//
+//  Created by whiis on 5/18/20.
+//  Copyright © 2020 kevtech. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
