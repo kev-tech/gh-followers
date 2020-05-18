f3c498c40062272dabed115454a2addee0b45c27
KC <kc@kevtech.co>
2020-05-18 17:12:25 -0400

    Add GFItemInfoView, GFFollowerItemVC, GFRepoItemVC, and GFItemInfoVC. Add profile and user info to  UserInfoVC.

---
+++ gh-followers/Views/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
@@ -0,0 +1,23 @@
+//
+//  GFFollowerItemVC.swift
+//  gh-followers
+//
+//  Created by whiis on 5/18/20.
+//  Copyright © 2020 kevtech. All rights reserved.
+//
+
+import UIKit
+
+class GFFollowerItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+}
