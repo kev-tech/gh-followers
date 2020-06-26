00cab73e747b60be3ff0ec231d40fd1e1aebd579
KC <kev@hey.com>
2020-06-25 20:47:46 -0400

    Commit before moving to Gitlab.

---
+++ gh-followers/Views/CustomViews/FavoriteCell.swift
@@ -0,0 +1,24 @@
+//
+//  FavoriteCell.swift
+//  gh-followers
+//
+//  Created by whiis on 5/20/20.
+//  Copyright © 2020 kevtech. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
