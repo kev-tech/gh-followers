3874c26d795b21f9b5e0449c63dcf82c205e5124
KC <kev@hey.com>
2020-06-25 20:51:52 -0400

    Update.

--- gh-followers/Views/CustomViews/FavoriteCell.swift
+++ gh-followers/Views/CustomViews/FavoriteCell.swift
@@ -9,16 +9,42 @@
 import UIKit
 
 class FavoriteCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    let reuseID = "favoriteCell"
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let username = GFTitleLabel(textAlignment: .center, fontSize: 26)
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
+    
+    private func configure() {
+        addSubview(avatarImageView)
+        addSubview(username)
+        
+        accessoryType = .disclosureIndicator
+        let padding:CGFloat = 12
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            avatarImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 60),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 60),
+            
+            username.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            username.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: padding),
+            username.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
+            username.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+    
+    func set(favorite: Follower) {
+        username.text = favorite.login
+        avatarImageView.downloadImage(from: favorite.avatarUrl)
+    }
 }
